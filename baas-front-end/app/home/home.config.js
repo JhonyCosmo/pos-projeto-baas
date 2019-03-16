7e8ec8fcca7fcd9d540c1986c1980bb999c63ce2
Maxwell Kenned <kenned123@gmail.com>
2019-03-16 11:30:52 -0300

    Adicionado component Home

---
+++ baas-front-end/app/home/home.config.js
@@ -0,0 +1,21 @@
+(function () {
+    'use strict';
+  
+    angular.module('baas-front-end').config(function ($stateProvider) {
+  
+        var homeState = {
+          name: 'app.home',
+          url: '/home',
+          views: {
+            mainContent: {
+              templateUrl: 'home/home.html',
+              controller: 'HomeCtrl',
+              controllerAs: 'vm'
+            }
+          }
+        };
+        
+        $stateProvider.state(homeState);  
+      })
+    })();
+    
